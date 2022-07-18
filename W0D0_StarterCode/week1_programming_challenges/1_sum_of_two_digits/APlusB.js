e90fa962257c58926dcef919bb99620e8bc21090
MCatapang <maocatap@gmail.com>
2022-07-18 01:32:06 -0700

    Created test version

--- W0D0_StarterCode/week1_programming_challenges/1_sum_of_two_digits/APlusB.js
+++ W0D0_StarterCode/week1_programming_challenges/1_sum_of_two_digits/APlusB.js
@@ -1,18 +1,26 @@
-var readline = require('readline');
+// var readline = require('readline');
 
-process.stdin.setEncoding('utf8');
-var rl = readline.createInterface({
-  input: process.stdin,
-  terminal: false
-});
+// process.stdin.setEncoding('utf8');
+// var rl = readline.createInterface({
+//   input: process.stdin,
+//   terminal: false
+// });
 
-rl.on('line', readLine);
+// rl.on('line', readLine);
 
-function readLine (line) {
-  if (line !== "\n") {
-    var a = parseInt(line.toString().split(' ')[0], 10);
-    var b = parseInt(line.toString().split(' ')[1], 10);
-    console.log(a + b);
-    process.exit();
-  }
+// function readLine (line) {
+//   if (line !== "\n") {
+//     var a = parseInt(line.toString().split(' ')[0], 10);
+//     var b = parseInt(line.toString().split(' ')[1], 10);
+//     console.log(a + b);
+//     process.exit();
+//   }
+// }
+
+// Test for Coursera's Grading System
+function twoSum(str) {
+    let numArr = parseInt(str).split(" ");
+    if(numArr.length == 2) {
+        return numArr[0] + numArr[1];
+    }
 }
